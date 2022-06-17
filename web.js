5cf9f538b81a46bd5cac0775dfed19f9e0d015fa
notabutb <notabutb@naver.com>
2022-06-17 13:26:24 +0900

    resume

--- web.js
+++ web.js
@@ -3,9 +3,16 @@ const app = express()
 const PORT = 8001
 app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs')
+
+app.get('/resume', (req, res) => {
+    res.render('resume')
+  })
+
 app.get('/', (req, res) => {
   res.render('index')
 })
+
+
 app.listen(PORT, () => {
     console.log(`server started on PORT ${PORT}`)
 })
